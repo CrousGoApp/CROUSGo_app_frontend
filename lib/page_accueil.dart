41d62be66dcbfaa469677cd64fab6693eb55b8c1
TeoVandroemme <teo.vandroemme08@gmail.com>
2023-08-31 09:57:10 +0200

    Première liaison avec le BACK (attention il faut changer l'adresse ip)

--- lib/page_accueil.dart
+++ lib/page_accueil.dart
@@ -1,5 +1,7 @@
-import 'package:crousgo/page_panier.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:crousgo/page_panier.dart';
 
 class PageAccueil extends StatefulWidget {
   const PageAccueil({Key? key}) : super(key: key);
@@ -10,20 +12,41 @@ class PageAccueil extends StatefulWidget {
 
 class PageAccueilState extends State<PageAccueil>
     with SingleTickerProviderStateMixin {
+
+  List<dynamic> jsonData = []; // Initialisation avec une liste vide
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData(); // Appel à la fonction fetchData() au moment de l'initialisation de la page
+  }
+
+  Future<void> fetchData() async {
+    final response = await http.get(Uri.parse('http://172.27.218.247:8080/crousgo_app_backend/allergens'));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        jsonData = json.decode(response.body); // Stocker les données dans la variable jsonData
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: <Widget>[
           Container(
-            color: const Color(0xFFFDF7EF), // Couleur de la bannière
+            color: const Color(0xFFFDF7EF),
             padding: const EdgeInsets.all(20.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 const SizedBox(width: 30.0),
                 const SizedBox(
-                  width: 100.0, // Ajustez cette largeur selon vos besoins
+                  width: 100.0,
                   child: Text(
                     'CrousGO',
                     style: TextStyle(
@@ -37,7 +60,7 @@ class PageAccueilState extends State<PageAccueil>
                   onTap: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => const PagePanier()), // Remplacez PagePanier() par le nom de votre classe de la page panier
+                      MaterialPageRoute(builder: (context) => const PagePanier()),
                     );
                   },
                   child: SizedBox(
@@ -51,17 +74,26 @@ class PageAccueilState extends State<PageAccueil>
               ],
             ),
           ),
-          const Column(
+          Column(
             children: [
-              SizedBox(
-                width: double.infinity,
-                child: Text("La Carte", textAlign: TextAlign.left, style: TextStyle(
-                  fontSize: 18.0,
-                  color: Colors.black,
-                ),),
+              Column(
+                children: jsonData.map((item) => Text(item['name'] ?? '')).toList(), // Afficher la propriété 'name' de chaque élément JSON
+              ),
+              const SizedBox(height: 20.0), // Espacement entre les données JSON et le texte "La Carte"
+              const Column(
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                    child: Text("La Carte", textAlign: TextAlign.left, style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.black,
+                    ),),
+                  ),
+                ],
               ),
             ],
-          ),
+          )
+
         ],
       ),
     );
