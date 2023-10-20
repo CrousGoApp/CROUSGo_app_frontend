c3cdcc0df1a69e55de5118373cc370e886836631
Etienne Maillot <tolliam.e+gitbash@gmail.com>
2023-10-20 14:00:15 +0200

    edit profile page

--- lib/pages/ProfilePage.dart
+++ lib/pages/ProfilePage.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:crousgo/pages/EditProfilePage.dart';
+import 'package:crousgo/pages/ProfilePage.dart';
 import 'package:crousgo/pages/page_OrderHistory.dart';
 import 'package:crousgo/pages/page_accueil.dart';
 import 'package:crousgo/pages/page_panier.dart';
@@ -147,7 +149,7 @@ class _ProfilePageState extends State<ProfilePage> {
                 }
               },
               style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFF06C167), // Couleur d'UberEats
+                backgroundColor: const Color(0xFF06C167), 
               ),
               child: const Text(
                 'Déconnexion',
@@ -166,7 +168,7 @@ class _ProfilePageState extends State<ProfilePage> {
                 );
               },
               style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFF06C167), // Couleur d'UberEats
+                backgroundColor: const Color(0xFF06C167), 
               ),
               child: const Text(
                 'Historique des commandes',
@@ -213,94 +215,4 @@ class _ProfilePageState extends State<ProfilePage> {
     );
   }
 }
-class EditProfilePage extends StatelessWidget {
-  final User? user;
 
-  EditProfilePage({required this.user});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: GestureDetector(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const PageAccueil()),
-            );
-          },
-          child: const Text(
-            'CrousGO',
-            style: TextStyle(
-              fontSize: 24.0,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
-          ),
-        ),
-        backgroundColor: const Color(0xFF06C167),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const PagePanier()),
-              );
-            },
-            icon: const Icon(
-              Icons.shopping_cart,
-              color: Colors.white,
-            ),
-          ),
-          IconButton(
-            icon: const Icon(Icons.account_circle),
-            onPressed: () {
-              // Naviguer vers la page de profil
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => ProfilePage()));
-            },
-          ),
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Modifier le Profil',
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 20.0),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  // Ajoutez des champs pour modifier le nom, l'e-mail, etc.
-                  // ...
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF06C167), // Couleur d'UberEats
-                      textStyle: const TextStyle(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                    onPressed: () {
-                      // Sauvegardez les modifications ici
-                    },
-                    child: const Text('Sauvegarder'),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
